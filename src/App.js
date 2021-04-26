6f019788b5c6ab8d4ed572ff04e2bb8821bc5da4
Monica Williams <jay.m.howard@gmail.com>
2021-04-26 00:22:48 -0500

    connect & console.log() networkgit add .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import React, { useEffect } from 'react';
+import Web3 from 'web3';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {loadBlockChainData()} )
+
+  const loadBlockChainData = async () => {
+     const web3 = new Web3(Web3.givenProvider || "ws://localhost:8546");
+    const network = await web3.eth.net.getNetworkType();
+    console.log("network:", network);
+
+  }
+
   return (
     <div className="App">
       <h1>Hello!</h1>
