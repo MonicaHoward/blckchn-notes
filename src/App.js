d5254319323dd5a73a6055828f2e76e91d3c8e32
Monica Williams <jay.m.howard@gmail.com>
2021-04-26 02:58:01 -0500

    refactored to latest syntax

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Web3 from 'web3';
 import './App.css';
 
 function App() {
-
-  useEffect(() => {loadBlockChainData()} )
-
-  const loadBlockChainData = async () => {
-     const web3 = new Web3(Web3.givenProvider || "ws://localhost:8546");
-    const network = await web3.eth.net.getNetworkType();
-    console.log("network:", network);
-
-  }
+  
+  window.ethereum.request({method: "eth_requestAccounts"});
+  const web3 = new Web3(window.ethereum);
+  const version = web3.version;
+  console.log(web3.version);
 
   return (
     <div className="App">
-      <h1>Hello!</h1>
+      <h1>Blockchain Notes App</h1>
+      <h2>Powered by {version}</h2>
     </div>
   );
 }
